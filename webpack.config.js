abb668bd798ed20ec2ca1cb473324b7943bfbe28
Frank Pagan <frank@cocreate.app>
2022-12-22 13:22:46 +0000

    fix: update demos and bump dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,6 @@ module.exports = {
     libraryExport: "default",
     library: ["CoCreate", "croppie"],
     globalObject: "this",
-    // publicPath: 'https://server.cocreate.app/CoCreateJS/dist/'
   },
 
   plugins: [
