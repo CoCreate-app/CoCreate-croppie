89ce79730224ff1cc8fd88025a6f4cd6ddff1269
frankpagan <frank@cocreate.app>
2023-06-06 12:48:44 -0600

    removed @cocreate/hosting

--- src/index.js
+++ src/index.js
@@ -3,169 +3,169 @@ import crud from '@cocreate/crud-client';
 import action from '@cocreate/actions';
 
 const CocreateCroppie = {
-  
-   debug : true,
-   selector_element : '[editor="croppie"]',
-   selector_croppie : '.croppie',
-   croppieElements : [],
-   croppieObjs : [],
-    
-    displayErrors : function(msg) {
-      if (this.debug)
-        console.error(msg)
+
+    debug: true,
+    selector_element: '[editor="croppie"]',
+    selector_croppie: '.croppie',
+    croppieElements: [],
+    croppieObjs: [],
+
+    displayErrors: function (msg) {
+        if (this.debug)
+            console.error(msg)
     },
-    
-    init : function() {
+
+    init: function () {
         this.croppieElements = document.querySelectorAll(this.selector_element);
-        for (let i=0; i < this.croppieElements.length; i++) {
-          this.initCroppie(this.croppieElements[i]);
+        for (let i = 0; i < this.croppieElements.length; i++) {
+            this.initCroppie(this.croppieElements[i]);
         }
     },
-    
-    initCroppie : function(el){
-        let cropieInit = (el.tagName != 'IMG') ?  el.querySelector(this.selector_croppie) : el;
+
+    initCroppie: function (el) {
+        let cropieInit = (el.tagName != 'IMG') ? el.querySelector(this.selector_croppie) : el;
         let fileInput = el.querySelector("input[type='file']");
         let objCroppie = {}
-        if (!cropieInit){
-          this.displayErrors("No genero Croppie = "+cropieInit)
-          return false
+        if (!cropieInit) {
+            this.displayErrors("No genero Croppie = " + cropieInit)
+            return false
         }
         let resizer = new croppie(cropieInit, {
-          boundary: { width: 300, height: 300 },
-          viewport: { width: 200, height: 200 },
-          showZoomer: true,
-          // enableResize: true,
-          enableOrientation: true,
-          enableZoom: true,
-          mouseWheelZoom: 'ctrl'
+            boundary: { width: 300, height: 300 },
+            viewport: { width: 200, height: 200 },
+            showZoomer: true,
+            // enableResize: true,
+            enableOrientation: true,
+            enableZoom: true,
+            mouseWheelZoom: 'ctrl'
         });
-        
-        objCroppie = {'croppie':el,'resizer':resizer}
-        
-        if (fileInput){
-          objCroppie["fileInput"] = fileInput
-          fileInput.addEventListener('change', function(e) {
-            if (this.files.length == 0) return;
-            let file = this.files[0];
-            let reader = new FileReader();
-            reader.onload = function(e) {
-              resizer.bind({
-                url: e.target.result
-              })
-            };
-            reader.readAsDataURL(file);
-          })
+
+        objCroppie = { 'croppie': el, 'resizer': resizer }
+
+        if (fileInput) {
+            objCroppie["fileInput"] = fileInput
+            fileInput.addEventListener('change', function (e) {
+                if (this.files.length == 0) return;
+                let file = this.files[0];
+                let reader = new FileReader();
+                reader.onload = function (e) {
+                    resizer.bind({
+                        url: e.target.result
+                    })
+                };
+                reader.readAsDataURL(file);
+            })
         }
-  
+
         this.croppieObjs.push(objCroppie);
     },
-    
-    saveCroppieCrud: async function(elCroppie) {
-      let name = elCroppie.getAttribute('name');
-      let data = elCroppie.dataset;
-      
-       if (typeof name === 'undefined' || name === '' || name ==null){
-        console.error("you need add attr [name] ");
-        return
-      }
-      
-      if (typeof data["collection"] === 'undefined' || data["collection"] === ''){
-        console.error("you need add attr [collection] ");
-        return
-      }
-      
-      let obj = this.croppieObjs.find((obj) => obj.croppie === elCroppie);
-      
-      let base64 = (elCroppie.tagName == 'IMG') ? await this.getCropResult(obj.resizer) : (obj.fileInput.files.length) ? await this.getCropResult(obj.resizer) : null
-      if (base64){
-        crud.createDocument({
-           collection:data["collection"],
-           document: {[name]:base64},
-         });
-      }else{
-        console.error("it is Empty, not save croppie in crud")
-      }
-      
-    },
-    
-  	__croppieUploadImageAction: function(btn) {
-  	  let croppie = btn.closest(this.selector_element);
-  	  if (!croppie ){
-          console.error("It needs to be inside an element "+this.selector_element+"")
-          return false
+
+    saveCroppieCrud: async function (elCroppie) {
+        let name = elCroppie.getAttribute('name');
+        let data = elCroppie.dataset;
+
+        if (typeof name === 'undefined' || name === '' || name == null) {
+            console.error("you need add attr [name] ");
+            return
         }
-      let fileInput = croppie.querySelector("input[type='file']");
-      if (!fileInput ){
-          console.error("You need in input file inside "+this.selector_element+"")
-          return false
+
+        if (typeof data["collection"] === 'undefined' || data["collection"] === '') {
+            console.error("you need add attr [collection] ");
+            return
         }
-      fileInput.click();
-  	  document.dispatchEvent(new CustomEvent('CroppieUploadImage', {
-  				detail: {}
-  			}))
-  	},
-  	
-    __croppieSaveAction: function(btn) {
-      
-      let croppie = btn.closest(this.selector_element);
-      let executeMultiple = false;
-  	  if (!croppie ){
-  	    //btn It is not within the parent tag
-  	    executeMultiple = true
-  	    let that = this;
-  	    let form = btn.closest('form');
-  	    let croppies = form.querySelectorAll(this.selector_element);
-  	    croppies.forEach(function(croppie) {
-          that.saveCroppieCrud(croppie);
-        });
-  	  }
-  	  
-  	  if (executeMultiple == false)
-  	    this.saveCroppieCrud(croppie)
-  	 
-  	  document.dispatchEvent(new CustomEvent('CroppieSave', {
-  				detail: {}
-  			}))
+
+        let obj = this.croppieObjs.find((obj) => obj.croppie === elCroppie);
+
+        let base64 = (elCroppie.tagName == 'IMG') ? await this.getCropResult(obj.resizer) : (obj.fileInput.files.length) ? await this.getCropResult(obj.resizer) : null
+        if (base64) {
+            crud.createDocument({
+                collection: data["collection"],
+                document: { [name]: base64 },
+            });
+        } else {
+            console.error("it is Empty, not save croppie in crud")
+        }
+
     },
-    
+
+    __croppieUploadImageAction: function (btn) {
+        let croppie = btn.closest(this.selector_element);
+        if (!croppie) {
+            console.error("It needs to be inside an element " + this.selector_element + "")
+            return false
+        }
+        let fileInput = croppie.querySelector("input[type='file']");
+        if (!fileInput) {
+            console.error("You need in input file inside " + this.selector_element + "")
+            return false
+        }
+        fileInput.click();
+        document.dispatchEvent(new CustomEvent('CroppieUploadImage', {
+            detail: {}
+        }))
+    },
+
+    __croppieSaveAction: function (btn) {
+
+        let croppie = btn.closest(this.selector_element);
+        let executeMultiple = false;
+        if (!croppie) {
+            //btn It is not within the parent tag
+            executeMultiple = true
+            let that = this;
+            let form = btn.closest('form');
+            let croppies = form.querySelectorAll(this.selector_element);
+            croppies.forEach(function (croppie) {
+                that.saveCroppieCrud(croppie);
+            });
+        }
+
+        if (executeMultiple == false)
+            this.saveCroppieCrud(croppie)
+
+        document.dispatchEvent(new CustomEvent('CroppieSave', {
+            detail: {}
+        }))
+    },
+
     readFile: function (file = {}, method = 'readAsText') {
-      const reader = new FileReader()
-      return new Promise((resolve, reject) => {
-        reader[method](file)
-        reader.onload = () => {
-          resolve(reader)
-        }
-        reader.onerror = (error) => reject(error)
-      })
-  },
-  
-  getCropResult : function(resizer) {
-    return new Promise((resolve, reject) => {
-      resizer.result('base64').then(function(base64) {
-      
-        resolve(base64);
-        
-      }).onerror = (error) => reject(error);
-   });
-  }
-  
+        const reader = new FileReader()
+        return new Promise((resolve, reject) => {
+            reader[method](file)
+            reader.onload = () => {
+                resolve(reader)
+            }
+            reader.onerror = (error) => reject(error)
+        })
+    },
+
+    getCropResult: function (resizer) {
+        return new Promise((resolve, reject) => {
+            resizer.result('base64').then(function (base64) {
+
+                resolve(base64);
+
+            }).onerror = (error) => reject(error);
+        });
+    }
+
 }//end class CocreateCroppie
 
 action.init({
-	name: "CroppieUploadImage",
-	endEvent: "CroppieUploadImage",
-	callback: (btn, data) => {
-		CocreateCroppie.__croppieUploadImageAction(btn);
-	}
+    name: "CroppieUploadImage",
+    endEvent: "CroppieUploadImage",
+    callback: (btn, data) => {
+        CocreateCroppie.__croppieUploadImageAction(btn);
+    }
 });
 
 
 action.init({
-	name: "CroppieSave",
-	endEvent: "CroppieSave",
-	callback: (btn, data) => {
-		CocreateCroppie.__croppieSaveAction(btn);
-	},
+    name: "CroppieSave",
+    endEvent: "CroppieSave",
+    callback: (btn, data) => {
+        CocreateCroppie.__croppieSaveAction(btn);
+    },
 });
 
 CocreateCroppie.init();
